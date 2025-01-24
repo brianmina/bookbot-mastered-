d7aa0a70626a6f4c2b2e49e4440111e6ac65d1eb
brianmina17 <brian.mina17@gmail.com>
2025-01-24 06:00:29 -0500

    counting character function in progress

--- main.py
+++ main.py
@@ -3,7 +3,24 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         words = file_contents.split()
-        print(len(words))
+        counter = count_words(words)
+        
+
+
+
+def count_words(words):
+    return len(words)
+
+
+def count_characters(text):
+    lowered_text = file_contents.lower()
+    return lowered_text
+
+#TODO:
+# fix and complete the character function
+# understand the conection between main and the other functions
+
+
 
 if __name__ == "__main__":
     main()
